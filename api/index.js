1d544ec3c863c902c8ebc3d0df5c4bae4240fdbf
Agustin Farias <96506530+fariasagustin3@users.noreply.github.com>
2022-12-12 16:43:49 -0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -6,6 +6,7 @@ const authRoute = require('./routes/auth')
 const productRoute = require('./routes/product')
 const cartsRoute = require('./routes/cart')
 const ordersRoute = require('./routes/order')
+const stripeRoute = require("./routes/stripe");
 const cors = require('cors')
 
 dotenv.config()
@@ -27,6 +28,7 @@ app.use('/api/auth', authRoute)
 app.use('/api/products', productRoute)
 app.use('/api/carts', cartsRoute)
 app.use('/api/orders', ordersRoute)
+app.use("/api/checkout", stripeRoute);
 
 app.listen(process.env.PORT || 5000, () => {
     console.log('Backend server is running')
--- api/routes/stripe.js
+++ api/routes/stripe.js
@@ -1,5 +1,7 @@
 const router = require('express').Router();
-const stripe = require('stripe')(process.env.STRIPE_KEY);
+const KEY = process.env.STRIPE_KEY
+const Stripe = require('stripe')
+const stripe = Stripe('sk_test_51LII2ZAVboyClKcxlXRQ0spDkky6x98RZgp9OucJaRDHqZEsGMVcCKsbpYDVajCq5LbIfRckGJChbaDZOq9tE9Ww00X5fT2ZyR')
 
 router.post('/payment', (req, res) => {
     stripe.charges.create({
